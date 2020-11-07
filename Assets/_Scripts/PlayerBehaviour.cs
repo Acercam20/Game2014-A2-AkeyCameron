b23cf45d6f28e9b1e8d093427448c0d59374a7db
Acercam20 <cameronakey@hotmail.ca>
2020-11-06 23:23:48 -0500

    Movement and animation update
    Added animation clips, animation manager, movement, jumping, a deathplane and a few other quality of life changes.

--- Assets/_Scripts/PlayerBehaviour.cs
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -7,18 +7,26 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     public Joystick joystick;
-    public float joystickSensitivity;
+    public float joystickHorizontalSensitivity;
+    public float joystickVerticalSensitivity;
     public float horizontalForce;
+    public float verticalForce;
     public float jumpForce;
     public float maximumVelocity;
+    public bool isGrounded;
+    public bool isJumping;
+    public Transform spawnPoint;
+
     private Rigidbody2D rb;
     private SpriteRenderer spriteRenderer;
+    private Animator m_animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        m_animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,16 +37,53 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void _Move()
     {
-        if (joystick.Horizontal > joystickSensitivity)
+        if (isGrounded)
         {
-            rb.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
-            spriteRenderer.flipX = false;
-        }
-        if (joystick.Horizontal < -joystickSensitivity)
-        {
-            rb.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
-            spriteRenderer.flipX = true;
+            if (joystick.Horizontal > joystickHorizontalSensitivity)
+            {
+                rb.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
+                spriteRenderer.flipX = false;
+                m_animator.SetInteger("AnimState", 1);
+            }
 
+            else if (joystick.Horizontal < -joystickHorizontalSensitivity)
+            {
+                rb.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
+                spriteRenderer.flipX = true;
+                m_animator.SetInteger("AnimState", 1);
+            }
+            
+            else if(!isJumping)
+            {
+                m_animator.SetInteger("AnimState", 0);
+            }
+
+            if (joystick.Vertical > joystickVerticalSensitivity)
+            {
+                rb.AddForce(Vector2.up * verticalForce * Time.deltaTime);
+                m_animator.SetInteger("AnimState", 2);
+                isJumping = true;
+            }
+            else
+            {
+                isJumping = false;
+            }
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        isGrounded = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("DeathPlane"))
+        transform.position = spawnPoint.position;
     }
 }
