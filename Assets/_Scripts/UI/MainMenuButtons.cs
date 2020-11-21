ff867b5a79de3c85660b1880e9d63adb28a47979
Acercam20 <cameronakey@hotmail.ca>
2020-11-21 16:50:03 -0500

    UI improvements
    Added custom UI assets, as well as functionality to the menu screens. Several unused folders and assets were tidied up as well.

---
+++ Assets/_Scripts/UI/MainMenuButtons.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnStartButtonPressed()
+    {
+        SceneManager.LoadScene("Platformer");
+    }
+
+    public void OnOptionsButtonPressed()
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+    public void OnQuitButtonPressed()
+    {
+        Application.Quit();
+    }
+
+    public void OnBackButtonPressed()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
